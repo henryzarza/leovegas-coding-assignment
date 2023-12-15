5f7a45937d3bac6535ac00fb7ce03bd039540594
Henry Zarza <henryacz7@gmail.com>
2023-12-16 00:02:33 +0200

    Fix search issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,76 @@
-import { useState } from 'react';
-import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from 'react-router-dom';
+import { useState, useCallback } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import Popup from 'reactjs-popup';
+
 import { fetchMovies } from './data/moviesSlice';
 import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants';
 import Header from './components/Header';
 import Movies from './pages/Movies';
 import Starred from './pages/Starred';
 import WatchLater from './pages/WatchLater';
+import AppContext from './contexts';
 import YouTubePlayer from './components/YoutubePlayer';
 import './app.scss';
 
 function App() {
   const dispatch = useDispatch();
-  const [searchParams, setSearchParams] = useSearchParams();
   const [videoKey, setVideoKey] = useState();
-  const [setOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
   const navigate = useNavigate();
-
-  const getSearchResults = (query) => {
-    if (query !== '') {
-      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=${query}`));
-      setSearchParams(createSearchParams({ search: query }));
-    } else {
-      dispatch(fetchMovies(ENDPOINT_DISCOVER));
-      setSearchParams();
-    }
-  };
 
   const searchMovies = (query) => {
     navigate('/');
-    getSearchResults(query);
+    dispatch(fetchMovies(query === '' ? ENDPOINT_DISCOVER : `${ENDPOINT_SEARCH}&query=${query}`));
   };
 
-  const getMovie = async (id) => {
-    const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`;
-
-    setVideoKey(null);
-    const videoData = await fetch(URL).then((response) => response.json());
+  const watchMovieTrailer = useCallback(async (movieId) => {
+    // TODO: implement a cache with Context to avoid making a requests for a movie the user previously requested
+    const videoData = await fetch(
+      `${ENDPOINT}/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos`
+    ).then((response) => response.json());
 
     if (videoData.videos && videoData.videos.results.length) {
-      const trailer = videoData.videos.results.find((vid) => vid.type === 'Trailer');
-      setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key);
+      const key =
+        videoData.videos.results[0].key ||
+        videoData.videos.results.find((vid) => vid.type === 'Trailer').key;
+      setVideoKey(key);
+      setModalOpen(true);
     }
-  };
+  }, []);
 
-  const viewTrailer = (movie) => {
-    getMovie(movie.id);
-
-    if (!videoKey) setOpen(true);
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setVideoKey(null);
   };
 
   return (
-    <div className='App'>
-      <Header
-        searchMovies={searchMovies}
-        searchParams={searchParams}
-        setSearchParams={setSearchParams}
-      />
+    <>
+      <Header searchMovies={searchMovies} />
 
-      <section className='container main-container'>
-        {videoKey ? (
-          <YouTubePlayer videoKey={videoKey} />
-        ) : (
-          <div style={{ padding: '30px' }}>
-            <h6>no trailer available. Try another movie</h6>
-          </div>
-        )}
+      <main className='container main-container'>
+        <AppContext.Provider value={watchMovieTrailer}>
+          <Routes>
+            <Route path='/' element={<Movies />} />
+            <Route path='/starred' element={<Starred />} />
+            <Route path='/watch-later' element={<WatchLater />} />
+            <Route path='*' element={<h1 className='main-title'>Page Not Found</h1>} />
+          </Routes>
+        </AppContext.Provider>
+      </main>
 
-        <Routes>
-          <Route path='/' element={<Movies viewTrailer={viewTrailer} />} />
-          <Route path='/starred' element={<Starred viewTrailer={viewTrailer} />} />
-          <Route path='/watch-later' element={<WatchLater viewTrailer={viewTrailer} />} />
-          <Route path='*' element={<h1 className='not-found'>Page Not Found</h1>} />
-        </Routes>
-      </section>
-    </div>
+      <Popup open={isModalOpen} onClose={handleCloseModal} closeOnDocumentClick>
+        <button
+          type='button'
+          aria-label='Close modal'
+          className='btn btn-outline-danger btn-close-modal'
+          onClick={handleCloseModal}
+        >
+          <i className='bi bi-x-lg' />
+        </button>
+        {videoKey && <YouTubePlayer videoKey={videoKey} />}
+      </Popup>
+    </>
   );
 }
 
