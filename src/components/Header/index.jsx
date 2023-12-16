5333158346a2c8138cacfbd59cd0433cab71372a
Henry Zarza <henryacz7@gmail.com>
2023-12-16 21:16:44 +0200

    Add search query to URL params

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,23 @@
 import { useCallback } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useSearchParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import debounce from '../../utils';
+import { debounce } from '../../utils';
 import './styles.scss';
 
 function Header({ searchMovies }) {
   const { starredMovies } = useSelector((state) => state.starred);
+  const [searchParams] = useSearchParams();
 
   const debouncedHandleSearch = useCallback(
-    debounce((e) => searchMovies(e.target.value), 300),
+    debounce((e) => {
+      // TODO: not sure about this behavior, needs to be discuss
+      if (e.target.value === '') {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+
+      searchMovies(e.target.value);
+    }, 300),
     []
   );
 
@@ -29,6 +37,8 @@ function Header({ searchMovies }) {
             className='form-control'
             placeholder='Search movies...'
             aria-label='Search movies'
+            // TODO: there is a known issue when the user goes to a page different than the home page, the input value is not being cleared.
+            defaultValue={searchParams.get('search')}
           />
         </div>
 
