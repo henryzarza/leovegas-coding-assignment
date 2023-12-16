e454dad349d6052b7accb84346d5bfb4fd8066a4
Henry Zarza <henryacz7@gmail.com>
2023-12-16 21:17:10 +0200

    Custom hook for intersection observer

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react';
+
 // Code from: https://www.joshwcomeau.com/snippets/javascript/debounce/
-const debounce = (callback, wait) => {
+export const debounce = (callback, wait) => {
   let timeoutId = null;
   return (...args) => {
     window.clearTimeout(timeoutId);
@@ -10,4 +12,27 @@ const debounce = (callback, wait) => {
   };
 };
 
-export default debounce;
+/**
+ * @param {object} elementRef DOM ref of the element to check the intersection
+ * @param {string} rootMargin margin around the root to ask if element is visible or not
+ * @returns isIntersecting if the element is visible or not
+ */
+export const useElementInView = (elementRef, rootMargin = '0px') => {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+    if (elementRef) observer.observe(elementRef);
+
+    return () => {
+      if (elementRef) observer.unobserve(elementRef);
+    };
+  }, [rootMargin, elementRef]);
+
+  return isIntersecting;
+};
